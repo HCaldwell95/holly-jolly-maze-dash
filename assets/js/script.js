ded58d683bc73aba8cbb1a49463d053733e2839c
Hayden Caldwell <hade_caldwell@msn.com>
2023-12-08 16:19:32 +0000

    Add row and column matrix function

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,7 +49,39 @@ class MazeBuilder {
     }
 
 }
+/**
+ * Creates a matrix with my specified number of rows and columns
+ * and fills each element with a value
+ */
+initArray(value) {
+    return new Array(this.rows).fill().map(() => new Array(this.cols).fill(value));
+}
 
+rand(min, max) {
+    return min + Math.floor(Math.random() * (1 + max - min));
+}
+
+posToSpace(x) {
+    return 2 * (x-1) + 1;
+}
+
+posToWall(x) {
+    return 2 * x;
+}
+
+/* Out of bounds */
+inBounds(r, c) {
+    if((typeof this.maze[r] == 'undefined') || (typeof this.maze[r][c] == 'undefined')) {
+        return false;
+    }
+    return true;
+}
+
+/* Source: https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array/12646864#12646864" */
+
+/**
+ * Picks a random element for each original array element and excludes it from the next draw
+ */
 
 
 
