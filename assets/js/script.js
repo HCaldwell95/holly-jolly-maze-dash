740f092e9fa3452e93339fff4abcf679fabb3409
Hayden Caldwell <hade_caldwell@msn.com>
2023-12-08 16:16:50 +0000

    Add js class MazeBuilder

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,57 @@
 
 
+class MazeBuilder {
+
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+
+        this.cols = 2 * this.width + 1;
+        this.rows = 2 * this.height + 1;
+
+        this.maze = this.initArray([]);
+
+        /* place initial walls */
+        this.maze.forEach((row, r) => {
+            row.forEach((cell, c) => {
+                switch(r)
+                {
+                    case 0:
+                    case this.rows - 1:
+                        this.maze[r][c] = ['wall'];
+                        break;
+
+                    default:
+                        if((r % 2) == 1) {
+                            if((c == 0) || (c == this.cols - 1)) {
+                                this.maze[r][c] = ['wall'];
+                            }
+                        } else if(c % 2 == 0) {
+                            this.maze[r][c] = ['wall'];
+                        }
+                }
+            });
+
+            if(r == 0) {
+                /* place tree in top row */
+                let treePos = this.posToSpace(this.rand(1, this.width));
+                this.maze[r][treePos] = ['tree', 'entrance'];
+            }
+
+            if(r == this.rows - 1) {
+                /* place entrance in bottom row */
+                let treePos = this.posToSpace(this.rand(1, this.width));
+                this.maze[r][treePos] = ['tree', 'entrance']
+            }
+        })
+        /* Start partitioning */
+        this.partition(1, this.height - 1, 1, this.width - 1);
+    }
+
+}
+
+
+
 
 
 
